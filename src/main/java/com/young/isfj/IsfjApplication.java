76bd069ece84d6fcd8da542d7469bc358bde5d6b
yozmngooddev <yozmngood.dev@gmail.com>
2023-01-22 11:43:51 +0900

    init commit isfj-app

---
+++ src/main/java/com/young/isfj/IsfjApplication.java
@@ -0,0 +1,13 @@
+package com.young.isfj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsfjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsfjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/young/isfj/IsfjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.young.isfj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsfjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
