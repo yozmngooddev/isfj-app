74f38666f75f011b20608ea3914356a160fa1a90
yozmngood.dev@gmail.com <yozmngood.dev@gmail.com>
2023-01-25 09:19:00 +0900

    mv package

--- src/main/java/com/young/isfj/sample/controller/TestController.java
+++ src/main/java/com/young/isfj/sample/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.young.isfj.controller;
+package com.young.isfj.sample.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
